96e16f65c771d87b99b679c51c24ce8ea74d7e6a
zylone <2289576153@qq.com>
2022-09-09 08:00:47 +0800

    give up using PID algrithm in task_2, after optimiaze(maximize) the parameters, we got a sample of 12s, which is good enough. Then we test the task_3, it's okay and we have a concrete idea now

--- build/sketch/mecanum_move.ino.cpp
+++ build/sketch/mecanum_move.ino.cpp
@@ -3,7 +3,7 @@
 #include <Wire.h>
 #include "QGPMaker_MotorShield.h"
 #include "QGPMaker_Encoder.h"
-#include "QGPMaker_IICSensorbar.h" 
+#include "QGPMaker_IICSensorbar.h"
 
 static int startPWM = 100;
 
@@ -13,18 +13,27 @@ volatile int PPR;
 volatile int gearratio;
 volatile int CPR;
 
-bool SLL,SL,SM,SR,SRR;
-enum{VERTICAL,HORIZEN,};
+bool SLL, SL, SM, SR, SRR;
+int detectNum;
+static enum { MID,
+              LEFT1,
+              RIGHT1,
+              LEFT2,
+              RIGHT2,
+              ALL,
+              UNDETECTED } movingStatus,
+    last_movingStatus;
+char *Tmove[]={"MID","LEFT1","RIGHT1","LEFT2","RIGHT2","ALL","UNDETECTED"};
 
 double kp, ki, kd;
-int pwmRestrict=100;
+int pwmRestrict = 100;
 
 QGPMaker_MotorShield AFMS = QGPMaker_MotorShield();
 
 // IICSensorBar I2C address :
 
-const byte IIC_ADDRESS = 0x3F;  
-SensorBar io; 
+const byte IIC_ADDRESS = 0x3F;
+SensorBar io;
 // Pin definition:
 const byte INPUT_PIN_S0 = 0;
 const byte INPUT_PIN_S1 = 1;
@@ -51,42 +60,54 @@ QGPMaker_Encoder Encoder4(4);
 /******************************************
                Motors
 ******************************************/
-#line 118 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 61 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void forward(int startPWM);
+#line 127 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void stopMoving();
-#line 129 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 138 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void straightForward();
-#line 140 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 150 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void show_rpm();
-#line 152 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 162 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void show_signal();
-#line 165 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 175 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 int calPID1(int velocity, int target);
-#line 192 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 202 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void pid_control1(int targetVolocity);
-#line 217 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 225 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void pid_control2(int targetVolocity);
-#line 238 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 245 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void pid_control3(int targetVolocity);
-#line 259 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 265 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void pid_control4(int targetVolocity);
-#line 280 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
-void pid_control_all(int targetVolocity1,int targetVolocity2,int targetVolocity3,int targetVolocity4);
-#line 291 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 285 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void pid_control_all(int targetVolocity1, int targetVolocity2, int targetVolocity3, int targetVolocity4);
+#line 297 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void init_pos();
-#line 298 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 305 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void fetch();
-#line 316 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
-void place();
 #line 324 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void place();
+#line 333 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void readPos();
-#line 340 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
-void lineFollow();
-#line 385 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 349 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void getStatus();
+#line 377 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void lineFollow1();
+#line 402 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void lineFollow(int vForward1,int vForward2,int vLeft1,int vRight1,int vLeft2,int vRight2,int lapse);
+#line 451 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void straightTrough(int n,void (*f)(int x));
+#line 472 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void set_middle(void (*f)(int x), int pwm, int lasting_time);
+#line 486 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void mirco_movement(void (*func)(int x), int pwm, int lasting_time);
+#line 495 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void setup();
-#line 413 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+#line 524 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
 void loop();
-#line 52 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
-void forward(int startPWM=200)
+#line 61 "c:\\Program Files\\arduino-1.8.19\\MYCAR\\mecanum_move\\mecanum_move.ino"
+void forward(int startPWM)
 {
   DCMotor_1->setSpeed(startPWM);
   DCMotor_1->run(FORWARD);
@@ -97,29 +118,29 @@ void forward(int startPWM=200)
   DCMotor_4->setSpeed(startPWM);
   DCMotor_4->run(FORWARD);
 }
-void turnLeft(int startPWM=200)
+void turnLeft(int startPWM = 200)
 {
   DCMotor_1->setSpeed(startPWM);
-  DCMotor_1->run(BACKWARD);
+  DCMotor_1->run(BRAKE);
   DCMotor_2->setSpeed(startPWM);
-  DCMotor_2->run(BACKWARD);
+  DCMotor_2->run(BRAKE);
   DCMotor_3->setSpeed(startPWM);
   DCMotor_3->run(FORWARD);
   DCMotor_4->setSpeed(startPWM);
   DCMotor_4->run(FORWARD);
 }
-void turnRight(int startPWM=200)
+void turnRight(int startPWM = 200)
 {
   DCMotor_1->setSpeed(startPWM);
   DCMotor_1->run(FORWARD);
   DCMotor_2->setSpeed(startPWM);
   DCMotor_2->run(FORWARD);
   DCMotor_3->setSpeed(startPWM);
-  DCMotor_3->run(BACKWARD);
+  DCMotor_3->run(BRAKE);
   DCMotor_4->setSpeed(startPWM);
-  DCMotor_4->run(BACKWARD);
+  DCMotor_4->run(BRAKE);
 }
-void moveLeft(int startPWM=200)
+void moveLeft(int startPWM = 200)
 {
   DCMotor_1->setSpeed(startPWM);
   DCMotor_1->run(BACKWARD);
@@ -130,7 +151,7 @@ void moveLeft(int startPWM=200)
   DCMotor_4->setSpeed(startPWM);
   DCMotor_4->run(FORWARD);
 }
-void moveRight(int startPWM=200)
+void moveRight(int startPWM = 200)
 {
   DCMotor_1->setSpeed(200);
   DCMotor_1->run(FORWARD);
@@ -141,7 +162,7 @@ void moveRight(int startPWM=200)
   DCMotor_4->setSpeed(200);
   DCMotor_4->run(BACKWARD);
 }
-void backward(int startPWM=200)
+void backward(int startPWM = 200)
 {
   DCMotor_1->setSpeed(200);
   DCMotor_1->run(BACKWARD);
@@ -155,15 +176,16 @@ void backward(int startPWM=200)
 void stopMoving()
 {
   DCMotor_1->setSpeed(0);
-  DCMotor_1->run(RELEASE);
+  DCMotor_1->run(BRAKE);
   DCMotor_2->setSpeed(0);
-  DCMotor_2->run(RELEASE);
+  DCMotor_2->run(BRAKE);
   DCMotor_3->setSpeed(0);
-  DCMotor_3->run(RELEASE);
+  DCMotor_3->run(BRAKE);
   DCMotor_4->setSpeed(0);
-  DCMotor_4->run(RELEASE);
+  DCMotor_4->run(BRAKE);
 }
-void straightForward(){
+void straightForward()
+{
   DCMotor_1->setSpeed(220);
   DCMotor_1->run(FORWARD);
   DCMotor_2->setSpeed(190);
@@ -198,7 +220,7 @@ void show_signal()
   delay(50);
 }
 
-static double bias, PWM, last_bias,last_bias2;
+static double bias, PWM, last_bias, last_bias2;
 int calPID1(int velocity, int target)
 {
 
@@ -207,7 +229,7 @@ int calPID1(int velocity, int target)
   Serial.print(",");
   Serial.print(last_bias);
   Serial.print(",");
-  PWM += kp * (bias - last_bias) + ki * bias + kd*(bias-2*last_bias+last_bias2);
+  PWM += kp * (bias - last_bias) + ki * bias + kd * (bias - 2 * last_bias + last_bias2);
 
   // Serial.print(PWM);
   // Serial.print(",");
@@ -220,7 +242,7 @@ int calPID1(int velocity, int target)
   {
     PWM = -pwmRestrict;
   }
-  last_bias2=last_bias2;
+  last_bias2 = last_bias2;
   last_bias = bias;
   Serial.print(PWM);
   Serial.print(",");
@@ -235,12 +257,11 @@ void pid_control1(int targetVolocity)
     DCMotor_1->setSpeed(pidOutput1 + startPWM);
     DCMotor_1->run(FORWARD);
   }
-  else if
-    (pidOutput1 == 0)
-    {
-      DCMotor_1->setSpeed(pidOutput1 + startPWM);
-      DCMotor_1->run(RELEASE);
-    }
+  else if (pidOutput1 == 0)
+  {
+    DCMotor_1->setSpeed(pidOutput1 + startPWM);
+    DCMotor_1->run(RELEASE);
+  }
   else
   {
     DCMotor_1->setSpeed(pidOutput1 + startPWM);
@@ -249,7 +270,6 @@ void pid_control1(int targetVolocity)
   Serial.print(pidOutput1 + startPWM);
   Serial.print(",");
   Serial.println(temp1);
-
 }
 void pid_control2(int targetVolocity)
 {
@@ -260,12 +280,11 @@ void pid_control2(int targetVolocity)
     DCMotor_2->setSpeed(pidOutput2 + startPWM);
     DCMotor_1->run(BACKWARD);
   }
-  else if
-    (pidOutput2 == 0)
-    {
-      DCMotor_2->setSpeed(pidOutput2 + startPWM);
-      DCMotor_2->run(RELEASE);
-    }
+  else if (pidOutput2 == 0)
+  {
+    DCMotor_2->setSpeed(pidOutput2 + startPWM);
+    DCMotor_2->run(RELEASE);
+  }
   else
   {
     DCMotor_2->setSpeed(pidOutput2 + startPWM);
@@ -281,12 +300,11 @@ void pid_control3(int targetVolocity)
     DCMotor_3->setSpeed(pidOutput3 + startPWM);
     DCMotor_3->run(BACKWARD);
   }
-  else if
-    (pidOutput3 == 0)
-    {
-      DCMotor_3->setSpeed(pidOutput3 + startPWM);
-      DCMotor_3->run(RELEASE);
-    }
+  else if (pidOutput3 == 0)
+  {
+    DCMotor_3->setSpeed(pidOutput3 + startPWM);
+    DCMotor_3->run(RELEASE);
+  }
   else
   {
     DCMotor_3->setSpeed(pidOutput3 + startPWM);
@@ -302,19 +320,19 @@ void pid_control4(int targetVolocity)
     DCMotor_4->setSpeed(pidOutput4 + startPWM);
     DCMotor_4->run(BACKWARD);
   }
-  else if
-    (pidOutput4 == 0)
-    {
-      DCMotor_4->setSpeed(pidOutput4 + startPWM);
-      DCMotor_4->run(RELEASE);
-    }
+  else if (pidOutput4 == 0)
+  {
+    DCMotor_4->setSpeed(pidOutput4 + startPWM);
+    DCMotor_4->run(RELEASE);
+  }
   else
   {
     DCMotor_4->setSpeed(pidOutput4 + startPWM);
     DCMotor_4->run(FORWARD);
   }
 }
-void pid_control_all(int targetVolocity1,int targetVolocity2,int targetVolocity3,int targetVolocity4){
+void pid_control_all(int targetVolocity1, int targetVolocity2, int targetVolocity3, int targetVolocity4)
+{
   pid_control1(targetVolocity1);
   pid_control2(targetVolocity2);
   pid_control3(targetVolocity3);
@@ -325,96 +343,200 @@ void pid_control_all(int targetVolocity1,int targetVolocity2,int targetVolocity3
               Servos
 ******************************************/
 
-void init_pos(){
-    Servo1->writeServo(45);
-    Servo2->writeServo(45);
-    Servo3->writeServo(90);
-    Servo4->writeServo(0); 
+void init_pos()
+{
+  Servo1->writeServo(45);
+  Servo2->writeServo(45);
+  Servo3->writeServo(90);
+  Servo4->writeServo(0);
 }
 
-void fetch(){
-    //Motion_1
-    Servo1->writeServo(90);
-    Servo2->writeServo(90);
-    delay(1000);
-    //Motion_2
-    Servo1->writeServo(150);
-    Servo3->writeServo(150);
-    delay(1000);
-    //Motion_3
-    Servo4->writeServo(60);
-    delay(1000);
-    //Motion_4
-    Servo1->writeServo(45);
-    Servo2->writeServo(45);
-    Servo3->writeServo(90);
+void fetch()
+{
+  // Motion_1
+  Servo1->writeServo(90);
+  Servo2->writeServo(90);
+  delay(1000);
+  // Motion_2
+  Servo1->writeServo(150);
+  Servo3->writeServo(150);
+  delay(1000);
+  // Motion_3
+  Servo4->writeServo(60);
+  delay(1000);
+  // Motion_4
+  Servo1->writeServo(45);
+  Servo2->writeServo(45);
+  Servo3->writeServo(90);
 }
 
-void place(){
-    Servo1->writeServo(150);
-    Servo2->writeServo(90);
-    Servo3->writeServo(150);
-    delay(1000);
-    Servo4->writeServo(0);
+void place()
+{
+  Servo1->writeServo(150);
+  Servo2->writeServo(90);
+  Servo3->writeServo(150);
+  delay(1000);
+  Servo4->writeServo(0);
 }
 
-void readPos(){
-    Serial.print("Position of S1,S2,S3,S4 is: ");
-    Serial.print(Servo1->readDegrees());
-    Serial.print(">");
-    Serial.print(Servo2->readDegrees());
-    Serial.print(">");
-    Serial.print(Servo3->readDegrees());
-    Serial.print(">");
-    Serial.println(Servo4->readDegrees());
+void readPos()
+{
+  Serial.print("Position of S1,S2,S3,S4 is: ");
+  Serial.print(Servo1->readDegrees());
+  Serial.print(">");
+  Serial.print(Servo2->readDegrees());
+  Serial.print(">");
+  Serial.print(Servo3->readDegrees());
+  Serial.print(">");
+  Serial.println(Servo4->readDegrees());
 }
-
 
 /******************************************
               Tasks
 ******************************************/
 
-void lineFollow(){
-  SRR=(io.digitalRead(INPUT_PIN_S0));
-  SR=(io.digitalRead(INPUT_PIN_S1));
-  SM=(io.digitalRead(INPUT_PIN_S2));
-  SL=(io.digitalRead(INPUT_PIN_S3));
-  SLL=(io.digitalRead(INPUT_PIN_S4));
+void getStatus()
+{
+  SRR = (io.digitalRead(INPUT_PIN_S0));
+  SR = (io.digitalRead(INPUT_PIN_S1));
+  SM = (io.digitalRead(INPUT_PIN_S2));
+  SL = (io.digitalRead(INPUT_PIN_S3));
+  SLL = (io.digitalRead(INPUT_PIN_S4));
+  detectNum=((SRR==0)+(SR==0)+(SM==0)+(SL==0)+(SLL==0));
+
+  if (SRR == HIGH && SR == HIGH && SM == LOW && SL == HIGH && SLL == HIGH)
+    movingStatus = MID;
+  else if (SRR == HIGH && SR == LOW && SM == HIGH && SL == HIGH && SLL == HIGH)
+    movingStatus = RIGHT1;
+  else if (SRR == HIGH && SR == HIGH && SM == HIGH && SL == LOW && SLL == HIGH)
+    movingStatus = LEFT1;
+  else if (SRR == LOW && SR == HIGH && SM == HIGH && SL == HIGH && SLL == HIGH)
+    movingStatus = RIGHT2;
+  else if (SRR == HIGH && SR == HIGH && SM == HIGH && SL == HIGH && SLL == LOW)
+    movingStatus = LEFT2;
+  else if (SRR == LOW && SR == LOW && SM == LOW && SL == LOW && SLL == LOW)
+    movingStatus = ALL;
+  else
+    movingStatus = UNDETECTED;
+  Serial.println(detectNum);
+  Serial.println(Tmove[movingStatus]);
+  Serial.println(Tmove[last_movingStatus]);
+}
+
+void lineFollow1()
+{
+  switch (movingStatus)
+  {
+  case MID:
+    forward(80);
+    break;
+  case LEFT1:
+    turnLeft(30);
+    break;
+  case RIGHT1:
+    turnRight(30);
+    break;
+  case LEFT2:
+    turnLeft(60);
+    break;
+  case RIGHT2:
+    turnRight(60);
+    break;
+  default:
+    forward(40);
+    break;
+  }
+}
+
+void lineFollow(int vForward1,int vForward2,int vLeft1,int vRight1,int vLeft2,int vRight2,int lapse)
+{
+  SRR = (io.digitalRead(INPUT_PIN_S0));
+  SR = (io.digitalRead(INPUT_PIN_S1));
+  SM = (io.digitalRead(INPUT_PIN_S2));
+  SL = (io.digitalRead(INPUT_PIN_S3));
+  SLL = (io.digitalRead(INPUT_PIN_S4));
 
   Serial.print("SO,S1,S2,S3,S4 status: ");
-	// Read the pin to print either 0 or 1
+  // Read the pin to print either 0 or 1
   Serial.print(SLL);
   Serial.print(SL);
   Serial.print(SM);
   Serial.print(SR);
   Serial.println(SRR);
 
-  if(SM==LOW){
-    forward(80);
+  if (SM == LOW)
+  {
+    forward(vForward1);
     Serial.println("forward");
   }
-  else if(SL==LOW){
-    turnLeft(30);
+  else if (SL == LOW)
+  {
+    turnLeft(vLeft1);
     Serial.println("left1");
   }
-  else if(SR==LOW){
-    turnRight(60);
+  else if (SR == LOW)
+  {
+    turnRight(vRight1);
     Serial.println("right1");
   }
-  else if(SLL==LOW){
-    turnLeft(30);
+  else if (SLL == LOW)
+  {
+    turnLeft(vLeft2);
     Serial.println("left2");
   }
-  else if(SRR==LOW){
-    turnRight(60);
+  else if (SRR == LOW)
+  {
+    turnRight(vRight2);
     Serial.println("right2");
   }
-  else{
-    forward(40);
+  else
+  {
+    forward(vForward2);
   }
-  delay(50);
+  delay(lapse);
 }
 
+//走过n条黑线
+void straightTrough(int n,void (*f)(int x))
+{
+  Serial.println("Test!");
+  int cnt = 0;
+  int num;
+  f(20);
+  while (cnt < n)
+  {
+    getStatus();
+    
+    if (detectNum>=3){
+      cnt++;
+      Serial.print("cnt=");
+      Serial.println(cnt);
+      delay(500);
+    }
+    // last_movingStatus=movingStatus;
+  }
+  stopMoving();
+}
+
+void set_middle(void (*f)(int x), int pwm, int lasting_time)
+{
+  while(1){
+    getStatus();
+    f(pwm);
+    if(movingStatus==MID){
+      stopMoving();
+      delay(lasting_time);
+      return;
+    }
+  }
+}
+
+//微调函数
+void mirco_movement(void (*func)(int x), int pwm, int lasting_time)
+{
+  func(pwm);
+  delay(lasting_time);
+}
 /******************************************
               SetupConfig
 ******************************************/
@@ -423,10 +545,11 @@ void setup()
 {
   AFMS.begin(50); // 50是啥我不清楚
   Serial.begin(9600);
-    if (!io.begin(IIC_ADDRESS))
+  if (!io.begin(IIC_ADDRESS))
   {
     Serial.println("Failed to communicate.");
-    while (1) ;
+    while (1)
+      ;
   }
   io.pinMode(INPUT_PIN_S0, INPUT);
   io.pinMode(INPUT_PIN_S1, INPUT);
@@ -436,24 +559,33 @@ void setup()
 
   init_pos();
   delay(1000);
-
+  
   pos = 0;
   rpm = 0;
   PPR = 12;
   gearratio = 90;
   CPR = (PPR * 4) * gearratio;
-  kp=1;
-  ki=0;
-  kd=0;
+  kp = 1;
+  ki = 0;
+  kd = 0;
 }
 
 void loop()
 {
-  //测试左右
-  // turnLeft(100);
-
-  //直线行驶
-  // straightForward();
-  // show_rpm();
-  lineFollow();
+  // moveLeft();
+  // delay(1000);
+  // straightTrough(5);
+  straightTrough(5,forward);
+  delay(1000);
+  // void set_middle(void *func(int x), int pwm, int lasting_time)
+  set_middle(moveLeft,40,2000);
+  backward(20);
+  delay(100);
+  stopMoving();
+  delay(1000);
+  fetch();
+  delay(1000);
+  place();
+  // void lineFollow(int vForward1,int vForward2,int vLeft1,int vRight1,int vLeft2,int vRight2,int lapse)
+  // lineFollow(100,40,30,30,60,60,50);
 }
